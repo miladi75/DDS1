1a671b32bbf7aa3fe117252d22697ffb95b9c2d3
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-09-26 20:27:18 +0200

    add functions modular multiplication and modular exponentiation

--- rsa.cpp
+++ rsa.cpp
@@ -1,13 +1,97 @@
 #include <iostream>
-#include <stdio.h>
-#include<math.h>
+#include <cmath>
+
+int32_t compute_modular_multiplicative_inverse(int32_t x, int32_t m)
+{
+    int32_t t = 0;
+    int32_t r = m;
+    int32_t next_t = 1;
+    int32_t next_r = x;
+
+    while (next_r != 0)
+    {
+        int32_t tmp = r / next_r;
+
+        int32_t saved_t = t;
+        t = next_t;
+        next_t = saved_t - tmp * next_t;
+
+        int32_t saved_r = r;
+        r = next_r;
+        next_r = saved_r - tmp * next_r;
+    }
+
+    if (r > 1)
+    {
+        exit(-1);
+    }
+    if (t < 0)
+    {
+        t = t + m;
+    }
+
+    return t;
+}
+
+// (x^e) % m
+int32_t modular_exp(int32_t x, int32_t e, int32_t m)
+{
+    if (m == 1)
+    {
+        return 0;
+    }
+
+    // OPTIONAL (only to compute with negative e)
+    if (e < 0)
+    {
+        x = compute_modular_multiplicative_inverse(x, m);
+        e = -e;
+    }
+    // END OPTIONAL
+
+    int32_t c = 1;
+    for (int32_t ep = 0; ep < e; ep++)
+    {
+        c = (c * x) % m;
+    }
+
+    return c;
+}
+
+// (x*e) % m
+int32_t modular_mult(int32_t x, int32_t e, int32_t m)
+{
+    int32_t next_x = x % m;
+    int32_t next_e = e % m;
+
+    return (next_x * next_e) % m;
+}
+
+int main()
+{
+    int32_t res_c = compute_modular_multiplicative_inverse(17, 120);
+    std::cout << res_c << std::endl;
+
+    std::cout << "MODULAR EXPONENTIATION" << std::endl;
+    int32_t res = modular_exp(17, -1, 120);
+    std::cout << res << ", expect 113" << std::endl;
+    res = modular_exp(50, 17, 143);
+    std::cout << res << ", expect 85" << std::endl;
+    res = modular_exp(85, 113, 143);
+    std::cout << res << ", expect 50" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "MODULAR MULTIPLICATION" << std::endl;
+    res = modular_mult(1421, 1423, 12);
+    std::cout << res << ", expect 11" << std::endl;
+}
 
 /**
  * @brief This moduel implements the high level RSA algorithm
  * 
  * @return int 
  */
-
+/*
 int power(int x, int y, int p)
 {
  
@@ -67,9 +151,5 @@ int main(){
     // std::cout << "phi(n) = " << phi << "\nd = " << d <<std::endl;
     // double lol = fmod(1/17, 120);
     // std::cout << lol << std::endl;   
-
-    
-
-
-
 }
+*/

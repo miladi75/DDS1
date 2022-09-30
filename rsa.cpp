449b24f7b58fb1500792e4fc36aba9f58a26367e
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-09-30 13:57:57 +0200

    use in-slide method algorithm

--- rsa.cpp
+++ rsa.cpp
@@ -1,54 +1,57 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdint>
 #include <cmath>
 
-#define BUFFER_SIZE 256
-
-// (x^e) % m
-int32_t modular_exp(int32_t x, int32_t e, int32_t m)
+uint64_t modular_mult(uint64_t a, uint64_t b, uint64_t n)
 {
-    if (m == 1)
+    uint64_t r, k;
+
+    k = floor(log2(b)) + 1;
+    r = 0;
+
+    for (uint64_t i = 0; i < k; i++)
     {
-        return 0;
+        uint64_t mask = 1 << (k - i - 1);
+        r = 2 * r + a * ((b & mask) >> (k - i - 1));
+        
+        if (r >= n)
+        {
+            r = r - n;
+        }
+        if (r >= n)
+        {
+            r = r - n;
+        }
     }
 
-    int32_t c = 1;
-    for (int32_t ep = 0; ep < e; ep++)
+    return r;
+}
+
+uint64_t modular_exp(uint64_t m, uint64_t e, uint64_t n)
+{
+    uint64_t c, p, k;
+
+    k = floor(log2(e)) + 1;
+    c = 1;
+    p = m;
+
+    for (uint64_t i = 0; i < k; i++)
     {
-        c = (c * x) % m;
+        uint64_t mask = 1 << i;
+        if ((e & mask) == mask)
+        {
+            c = modular_mult(c, p, n);
+        }
+        p = modular_mult(p, p, n);
     }
 
     return c;
 }
 
-void encrypt(int32_t e, int32_t n, int32_t msg_plain[BUFFER_SIZE], int32_t msg_encr[BUFFER_SIZE])
-{
-    for (size_t i = 0; i < BUFFER_SIZE; i++)
-    {
-        msg_encr[i] = modular_exp(msg_plain[i], e, n);
-    }
-}
-
-void decrypt(int32_t d, int32_t n, int32_t msg_encr[BUFFER_SIZE], int32_t msg_plain[BUFFER_SIZE])
-{
-    for (size_t i = 0; i < BUFFER_SIZE; i++)
-    {
-        msg_plain[i] = modular_exp(msg_encr[i], d, n);
-    }
-}
-
 int main()
 {
-    std::cout << "ENCRYPT" << std::endl;
-    int32_t msg_plain[BUFFER_SIZE] = {0};
-    int32_t msg_encr[BUFFER_SIZE] = {0};
-    
-    msg_plain[0] = 19;
-    encrypt(5, 119, msg_plain, msg_encr);
-    std::cout << msg_encr[0] << std::endl;
-
-    msg_plain[0] = 0;
-
-    std::cout << "DECRYPT" << std::endl;
-    decrypt(77, 119, msg_encr, msg_plain);
-    std::cout << msg_plain[0] << std::endl;
+    uint64_t res = modular_exp(19, 5, 119);
+    printf("%lu\n", res);
+    res = modular_exp(66, 77, 119);
+    printf("%lu\n", res);
 }

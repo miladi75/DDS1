5dcbb6a69801106c3aa53f9618fd8cf1eea9c001
Magnus Oddstoel <magnus.oddstoel@ubiqaerospace.com>
2022-10-25 15:32:12 +0200

    changed file locations

--- rsa_integration_kit/Exponentiation/source/PIPO_shift_register.vhd
+++ rsa_integration_kit/Exponentiation/source/PIPO_shift_register.vhd
@@ -23,15 +23,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity PIPO_shift_register is
     Port ( d : in STD_LOGIC_VECTOR (255 downto 0);
            clk : in STD_LOGIC;
@@ -44,6 +35,13 @@ end PIPO_shift_register;
 architecture rtl of PIPO_shift_register is
 
 begin
-    process(
+    process(clk, reset_n)
+    begin
+    if (reset_n = '0') then
+        q <= (others => '0');
+    elsif(clk'event and clk = '1') then
+        q <= STD_LOGIC_VECTOR(shift_left(unsigned(d), 1));
+    end if;
+    end process;
 
 end rtl;

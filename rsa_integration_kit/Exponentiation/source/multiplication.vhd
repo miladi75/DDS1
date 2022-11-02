1f5f877f395521aade0018ed6d431c09811fcaf2
S. M. Husseini <44436360+miladi75@users.noreply.github.com>
2022-11-02 19:12:59 +0100

    LOTS of bugs to be fixed

--- rsa_integration_kit/Exponentiation/source/multiplication.vhd
+++ rsa_integration_kit/Exponentiation/source/multiplication.vhd
@@ -18,6 +18,7 @@
 -- 
 ----------------------------------------------------------------------------------
 
+-- A comment line
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -33,38 +34,64 @@ entity multiplication is
         mul_a       : in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
         
         mul_b       : in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
-        c_enable    : in STD_LOGIC;
+        cnt_en      : in STD_LOGIC;
         input_n     : in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
         out_mul     : out STD_LOGIC_VECTOR(C_block_size-1 downto 0);
         done_mul    : out STD_LOGIC
         );
 
 
-
 end multiplication;
     
 
 
-architecture Behavioral of multiplication is
+architecture rtl of multiplication is
+    signal counter_in : std_logic_vector(C_block_size-1 downto 0);
     signal counter_out : STD_LOGIC_VECTOR(C_block_size-1 downto 0);
-    --component counter
     
-    --port map(
-    --coutner_out => y,
-    --y =>coutner_out ,
-    
-    --);
+    signal loop_done : std_logic;
+    signal k : std_logic_vector(integer(ceil(unsigened())-1 downto 0);
+    signal mux_sel : std_logic_vector(1 downto 0);
+    signal out_mux_1 : std_logic_vector(C_block_size-1 downto 0);
 
-begin 
-    COUNTER : entity work.counter
-    port map (
-      y => counter_out,
-     clk => i_clk,
-    reset_n => reset_n,
-    cnt_en => c_enable 
-    );  
+    
+    signal mul_exponent : std_logic;
+
+process(reset_n)
+begin
+    if reset_n = '0' then
+        mul_exponent <= '0';
+        out_mul <= (others => '0');
+        done_mul <= '0';
+    elsif rising_edge(i_clk) then
+        if cnt_en = '1' then
+            mul_exponent <= mul_a(C_block_size-1);
+            out_mul <= mul_a * mul_b;
+            done_mul <= '1';
+        end if;
+    end if;
+end process;
+
+process(mux_sel)
+begin
+    if counter_out = '0' then
+        mux_sel <= '0';
+    elsif counter_out <= '1' then
+        mux_sel <= '1';
+
+    end if;
+end process;
+
  
+COUNTER : entity work.counter
+    port map (
+        y => counter_out,
+        clk => i_clk,
+        reset_n => reset_n,
+        cnt_en => cnt_en );  
+process()
+        
         
       
     
-end Behavioral;
+end rtl;

ddcfe815bdf7ba0a2894c7298c513f62d69706f8
Magnus Oddstoel <magnus.oddstoel@ubiqaerospace.com>
2022-11-23 19:12:11 +0100

    added demux

---
+++ rsa_integration_kit/RSA_accelerator/source/demux1xN.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 25.10.2022 15:20:53
+-- Design Name: 
+-- Module Name: muxNx1 - muxNx1Behav
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.math_real.ceil;
+use IEEE.math_real.log2;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity demux1xN is
+    generic (
+		size : integer := 256
+	);
+    port (
+        input : in STD_LOGIC;
+        sel : in INTEGER range 0 to size-1;
+        output : out STD_LOGIC_VECTOR(0 to size-1)
+    );
+end demux1xN;
+
+architecture demux1xNBehav of demux1xN is
+begin
+    output <= (sel => input, others => '0');
+end demux1xNBehav;
